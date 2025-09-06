ef05ebfdfdc7ab0abd43a9df66f3bacd4201fb57
Roberto Jimenez <robertojj2006@gmail.com>
2025-09-06 18:10:01 +0200

    colores y demás

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function drawMatrix() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = "#00ffcc";
+  ctx.fillStyle = "#00ff0dff";
   ctx.font = fontSize + "px monospace";
 
   for (let i = 0; i < drops.length; i++) {
